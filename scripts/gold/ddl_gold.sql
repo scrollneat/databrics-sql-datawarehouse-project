f39a3a95d9894c401652fa3ca10675177b045249
scrollneat <barathiselvanofficial@gmail.com>
2026-02-24 23:55:46 +0000

    update

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -1,16 +1,42 @@
+/*
+===============================================================================
+DDL Script: Create Gold Views
+===============================================================================
+Script Purpose:
+    This script creates views for the Gold layer in the data warehouse. 
+    The Gold layer represents the final dimension and fact tables (Star Schema)
+
+    Each view performs transformations and combines data from the Silver layer 
+    to produce a clean, enriched, and business-ready dataset.
+
+Usage:
+    - These views can be queried directly for analytics and reporting.
+===============================================================================
+*/
+
+-- =============================================================================
+-- Create Dimension: gold.dim_customers
+-- =============================================================================
+
+CREATE OR REPLACE VIEW datawarehouse.gold.dim_customers as
 SELECT 
-cc.cst_id AS CUSTOMER_ID,
-cc.cst_key AS CUSTOMER_NUMBER,
-cc.cst_firstname AS FIRSTNAME,
-cc.cst_lastname AS LAST_NAME,
-cc.cst_marital_status AS MARITAL_STATUS, 
-cc.cst_gndr AS GENDER,
-cc.cst_create_date AS CREATE_DATE,
-ec.cid ,
-ec.bdate AS BIRTH_DATE,
-ec.gen,
-el.cid,
-el.cntry AS COUNTRY
+  cc.cst_id AS CUSTOMER_ID,
+  cc.cst_key AS CUSTOMER_NUMBER,
+  cc.cst_firstname AS FIRSTNAME,
+  cc.cst_lastname AS LAST_NAME,
+  el.cntry AS COUNTRY,
+  cc.cst_marital_status AS MARITAL_STATUS, 
+  CASE 
+    WHEN cc.cst_gndr != 'NA' THEN cc.cst_gndr -- CRM is the primary source for gender
+    ELSE COALESCE(ec.gen, 'NA') -- Fallback to ERP data
+  END AS GENDER,
+  ec.bdate AS BIRTH_DATE,
+  cc.cst_create_date AS CREATE_DATE
 FROM datawarehouse.silver.crm_cust_info cc
-LEFT JOIN datawarehouse.silver.erp_cust_az12 ec ON cc.cst_id = CAST(ec.cid AS STRING)
-LEFT JOIN datawarehouse.silver.erp_loc_a101 el ON el.cid = cc.cst_id;
+LEFT JOIN datawarehouse.silver.erp_cust_az12 ec ON cc.cst_key = ec.cid
+LEFT JOIN datawarehouse.silver.erp_loc_a101 el ON el.cid = cc.cst_key;
+
+
+-- =============================================================================
+-- Create Dimension: gold.dim_products
+-- =============================================================================
